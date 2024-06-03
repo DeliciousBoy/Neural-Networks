d62b2582be3e303eb767c5e70a2515f37762f96a
DeliciousBoy <wittayatangdorn.gide@gmail.com>
2024-06-03 09:07:05 +0700

    create neural network packages

---
+++ neural_networks/layer_dense.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Layer_Dense:
+    def __init__(self, n_inputs: int, n_neurons: int) -> None:
+        """
+        Initialize the layer with random weights and biases
+
+        Parameters:
+            n_inputs  : Number of input features.
+            n_neurons : Number of neurons in the layer.
+        """
+        # Initialize weights with a standard normal distribution scaled by 0.10
+        self.weight = 0.10 * np.random.randn(n_inputs, n_neurons) 
+        # Initialize biases with zeros
+        self.biases = np.zeros((1, n_neurons))
+  
+    def forward(self, inputs: list[list[int]]) -> None:
+        """
+        Perform a forward pass through the layer.
+
+        Parameters:
+            inputs  : Input data.
+        """
+        self.output = np.dot(inputs, self.weight) + self.biases
--- p1.py
+++ p1.py
@@ -6,26 +6,20 @@ X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
-
-class Layer_Dense:
-    def __init__(self, n_inputs: int, n_neurons: int) -> None:
-        """
-        Initialize the layer with random weights and biases
-
-            Parameters:
-                n_inputs : Number of input features.
-                n_neurons : Number of neurons in the layer.
-        """
-        self.weight = 0.10 * np.random.randn(n_inputs, n_neurons) # normalize
-        self.biases = np.zeros((1, n_neurons))
-
+class Activation_ReLU:
     def forward(self, inputs: list[list[int]]) -> None:
-        self.output = np.dot(inputs, self.weight) + self.biases
+        self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+        
+# layer1 = Layer_Dense(4, 5)
+# layer2 = Layer_Dense(5, 2)
 
-layer1.forward(X)
-print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+# layer1.forward(X)
+# print(layer1.output)
+# layer2.forward(layer1.output)
+# print(layer2.output)
+
+
+# input = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+# output = [i for i in input if max(0, i)]
+# print(output)
