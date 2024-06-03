60004204303cfa3840210f97c398c0b79ab46521
DeliciousBoy <outdata.gide@gmail.com>
2024-06-04 00:31:48 +0700

    add create spiral_data

--- neural_networks/layer_dense.py
+++ neural_networks/layer_dense.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Layer_Dense:
-    def __init__(self, n_inputs: int, n_neurons: int) -> None:
+    def __init__(self, n_inputs: int, n_neurons: int):
         """
         Initialize the layer with random weights and biases
 
@@ -13,8 +13,8 @@ class Layer_Dense:
         self.weight = 0.10 * np.random.randn(n_inputs, n_neurons) 
         # Initialize biases with zeros
         self.biases = np.zeros((1, n_neurons))
-  
-    def forward(self, inputs: list[list[int]]) -> None:
+
+    def forward(self, inputs: np.ndarray) -> None:
         """
         Perform a forward pass through the layer.
 
